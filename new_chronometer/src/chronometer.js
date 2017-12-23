6e0b71099e4d30bcede6353c610243c13dadb060
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2017-12-23 15:31:09 -0200

    Constructor Development

--- new_chronometer/src/chronometer.js
+++ new_chronometer/src/chronometer.js
@@ -1,5 +1,13 @@
 class Chronometer {
+  constructor(config) {
+    if (config === undefined) {
+      throw new Error('A configuration object must be passed on object instance');
+    }
 
+    this.hours = config.hours;
+    this.minutes = config.minutes;
+    this.seconds = config.seconds;
+  }
 }
 
 export default Chronometer;
--- new_chronometer/src/tests/chronometer.test.js
+++ new_chronometer/src/tests/chronometer.test.js
@@ -1,7 +1,30 @@
 import Chronometer from '../chronometer';
 
-describe('Chronometer', () => {
-  test('new Chronometer() should return a new instance', () => {
-    expect(new Chronometer()).toBeInstanceOf(Chronometer);
+describe('Smoke Tests Chronometer', () => {
+  test('new Chronometer({}) should return a new instance', () => {
+    expect(new Chronometer({}))
+      .toBeInstanceOf(Chronometer);
+  });
+
+  test('new Chronometer({ hours: 10 }) should return { hours: 10 }', () => {
+    expect(new Chronometer({ hours: 10 }))
+      .toEqual({ hours: 10 });
+  });
+
+  test('new Chronometer({ hours: 10, minutes: 5 }) should return { hours: 10, minutes: 5 }', () => {
+    expect(new Chronometer({ hours: 10, minutes: 5 }))
+      .toEqual({ hours: 10, minutes: 5 });
+  });
+
+  test('new Chronometer({ hours: 10, minutes: 5, seconds: 3 }) should return { hours: 10, minutes: 5, seconds: 3 }', () => {
+    expect(new Chronometer({ hours: 10, minutes: 5, seconds: 3 }))
+      .toEqual({ hours: 10, minutes: 5, seconds: 3 });
+  });
+});
+
+describe('Chronometer Exceptions', () => {
+  test('new Chronometer() should throw an Error', () => {
+    expect(() => new Chronometer())
+      .toThrow('A configuration object must be passed on object instance');
   });
 });
