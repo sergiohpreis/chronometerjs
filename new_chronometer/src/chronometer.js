8c66664c4ecfaeef924a0dbb04cfc59ddcd44fe6
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2017-12-23 18:26:51 -0200

    Reseting

--- new_chronometer/src/chronometer.js
+++ new_chronometer/src/chronometer.js
@@ -1,13 +1,5 @@
 class Chronometer {
-  constructor(config) {
-    if (config === undefined) {
-      throw new Error('A configuration object must be passed on object instance');
-    }
 
-    Object.keys(config).forEach((item) => {
-      this[item] = config[item];
-    });
-  }
 }
 
 export default Chronometer;
--- new_chronometer/src/index.js
+++ new_chronometer/src/index.js
@@ -1,7 +1,5 @@
 import Chronometer from './chronometer';
 
 console.log(new Chronometer({
-  hours: 5,
-  minutes: 10,
-  seconds: 58,
+  schema: ['10']
 }));
