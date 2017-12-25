269b17d505cb4a81a2b72201c2cc1c9d9755fdd5
Sergio Henrique  Pereira Reis <sergio.hpreis@gmail.com>
2017-12-25 16:07:26 -0200

    Refactoring and accepting hours

--- new_chronometer/src/chronometer.js
+++ new_chronometer/src/chronometer.js
@@ -1,4 +1,5 @@
 import CONFIG from './constants';
+import numberToString from './utils';
 
 class Chronometer {
   constructor(config) {
@@ -36,20 +37,31 @@ class Chronometer {
     });
   }
 
-  countdown(seconds) {
-    if (seconds === 0) {
+  countdown() {
+    if (this.seconds === 0 && this.minutes !== undefined) {
+      this.minutes = this.minutes - 1;
+      this.seconds = 60;
+    }
+
+    if (this.minutes === -1 && this.hours !== undefined) {
+      this.hours = this.hours - 1;
+      this.minutes = 59;
+    }
+
+    if ((this.seconds === 0 && this.minutes === undefined) ||
+    (this.minutes === 0 && this.hours === undefined)) {
       return;
     }
 
     setTimeout(() => {
-      this.seconds = seconds - 1;
+      this.seconds = this.seconds - 1;
       this.updateCurrentTime();
       this.countdown(this.seconds);
     }, 1000);
   }
 
   updateCurrentTime() {
-    this.currentTime = `00:00:${this.seconds}`;
+    this.currentTime = `${this.hours ? numberToString(this.hours) : '00'}:${this.minutes ? numberToString(this.minutes) : '00'}:${numberToString(this.seconds)}`;
   }
 
   start() {
